38bdfa11ce57ae2da66c77ccdd5f57caf6e8d426
YoungOops <143402260+YoungOops@users.noreply.github.com>
2023-10-22 03:12:10 +0900

    gitgit

--- moviesearch.js
+++ moviesearch.js
@@ -5,7 +5,7 @@ const options = {
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZDQwZTIwNzJhYWY0MjM1ZjA4NmNmYjM4ODllZjU2OSIsInN1YiI6IjY1MmY2NTdjMGNiMzM1MTZmZWM5ZTc2YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hcY72thbhWcl-T2PtLjZyBMRfyPjnf554i-7OEIiRUw'
     }
   };
-  
+
   fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
     .then(response => response.json())
     .then(response => {
@@ -21,29 +21,32 @@ const options = {
 
     const map = new Map(); //맵 선언함 타이틀 키값 밸류는 카드를 넣을 것임
 
+//map 이라는 메서드가 따로 있고, new map은 일급객체가 있다고함 나중에 알아볼것 ********
+
     const container= document.getElementById("cardContainer");
     let createCard = (data) => {
-        const card = document.createElement("div");
-        container.appendChild(card); //컨테이너 만든 위치에 자식으로 카드를 넣는다. 
-        
-        const title = document.createElement("div");
-        card.appendChild(title); // 타이틀은 카드에!
+        const card = document.createElement("div"); // 카드를 만들자
+        card.classList.add("movieCard"); //클래스 리스트
+        container.appendChild(card); // 컨테이너 만든 위치에 자식으로 카드를 넣는다. 
+
+        const title = document.createElement("div"); // div 이놈은 html 여기서 쓰니까 암튼 말해주는 거임
+        card.appendChild(title); // 타이틀은 카드에! 카드의 자식 타이틀 만든다.
         title.innerText = data.title;
 
         const image = document.createElement("img");// 이미지는 이렇게
-        card.appendChild(image); // 얘도 카드에 !
+        card.appendChild(image); // 얘도 카드에 ! 카드 안에 있는 자식 이미지 만든다.
         image.src = "https://image.tmdb.org/t/p/original/" + data.backdrop_path; //이게 key 값 데이터. 키값
-        image.style.width = "300px";
+        image.style.width = "380px";
 
         const info = document.createElement("div");
-        card.appendChild(info); // 인포도 카드에!
+        card.appendChild(info); // 인포도 카드에! 카드 안에 있는 자식 인포 만든다
         info.innerText = data.overview; // 크롬에서 가져와야함 키 확인해서
 
         const voteAverage = document.createElement("div");
         card.appendChild(voteAverage); // 평점 카드에!
         voteAverage.innerText = data.vote_average; // 평점도 f12 에서 가져오기
 
-        map.set(data.title, card);
+        map.set(data.title, card); // 얘네를 묶어서 한 세트로 쳐준다.
         const id = data.id;
         card.addEventListener("click",()=>{
             alert(id);
@@ -61,9 +64,8 @@ const options = {
             }
             else {
                 map.get(title).style.display = 'none';
-    
+
             }
-    
         }
     }
     searchButton.addEventListener("click",()=>{
@@ -73,8 +75,8 @@ const options = {
     searchInput.focus(); //커서 깜빡깜빡거리게
 
     searchInput.addEventListener("keyup",(event)=>{
-        
-        if(event.key==="enter"){ // enter 대소문자 구분 없음
+
+        if(event.key==="Enter"){ // enter 대소문자 구분 없음
             search();
         }
     });
